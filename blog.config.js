b397986d703427e097ceeadd12871dbf2ce09d1e
Altjasonspeed <59773981+Altjasonspeed@users.noreply.github.com>
2024-06-27 22:46:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ALTjasonspeed', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '挺多神奇的东西的', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://alt-js.github.io/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog, Project, SDVX', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
